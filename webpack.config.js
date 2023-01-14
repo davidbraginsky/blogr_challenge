f615fa68d5a4eb1ccd35d4ba91f9c7d3c2647ff2
David <braginsky.david@yandex.ru>
2023-01-14 21:03:15 +0300

    updated webpack config file

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,58 @@
-import { resolve as _resolve } from "path";
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require("path");
 
-export const mode = "development";
-export const entry = "./src/index.js";
-export const output = {
-  path: _resolve(__dirname, "public"),
-  filename: "main.js",
+module.exports = {
+  mode: "development",
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+  },
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: ["./public"],
+    open: false,
+    hot: true,
+    liveReload: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+    ],
+  },
 };
-export const target = "web";
-export const devServer = {
-  port: "3000",
-  static: ["./public"],
-  open: false,
-  hot: true,
-  liveReload: true,
-};
-export const resolve = {
-  extensions: [".js", ".jsx", ".json"],
-};
-export const module = {
-  rules: [
-    {
-      test: /\.(js|jsx)$/,
-      exclude: /node_modules/,
-      use: "babel-loader",
-    },
-  ],
-};
+
+// export const mode = "development";
+// export const entry = "./src/index.js";
+// export const output = {
+//   path: _resolve(__dirname, "public"),
+//   filename: "main.js",
+// };
+// export const target = "web";
+// export const devServer = {
+//   port: "3000",
+//   static: ["./public"],
+//   open: false,
+//   hot: true,
+//   liveReload: true,
+// };
+// export const resolve = {
+//   extensions: [".js", ".jsx", ".json"],
+// };
+// export const module = {
+//   rules: [
+//     {
+//       test: /\.(js|jsx)$/,
+//       exclude: /node_modules/,
+//       use: "babel-loader",
+//     },
+//   ],
+// };
